1b1fde9466e23344098c32dbc5bed3b5117e6ec8
syedanastanweer <syedanastanwer@gmail.com>
2024-09-19 22:21:13 +0500

    commit1

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Navbar</title>
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <section class="hero-section">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Resources</a>
+                        </li>
+                        <!-- <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link head-btn" href="#" tabindex="-1" aria-disabled="true">Get Started</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="header-section">
+            <h1>Optavist</h1>
+            <h2>30 days to</h2>
+            <img src="assets/img/banner-txt-1.png">
+            <h3>5 steps to fast track your success!</h3>
+        </div>
+        </div>
+    </section>
+
+    <section class="sectwo">
+        <div class="upper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Your Coaching Journey</h2>
+                        <h3>Start Here</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="midder-one">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="iframe-container">
+                            <iframe src="https://www.youtube.com/embed/XHOmBV4js_E?si=678QVhubK_wKmtHp"
+                                title="YouTube video player"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="midder-two">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Your 5 Simple Steps to</h2>
+                        <h3>Sucess</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="midder-three">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="secimgone">
+                            <img src="assets/img/secimgone.png">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10 offset-md-2">
+                        <div class="secimgtwo">
+                            <img src="assets/img/secimgtwo.png">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="secimgthree">
+                            <img src="assets/img/secimgthree.png">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10 offset-md-2">
+                        <div class="secimgfour">
+                            <img src="assets/img/secimgfour.png">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="secimgfive">
+                            <img src="assets/img/secimgfive.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="footer">
+        <h2>
+            Congratulations!
+        </h2>
+        <h3>
+            You did it!
+        </h3>
+    </section>
+
+    <!-- Bootstrap JS and Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
